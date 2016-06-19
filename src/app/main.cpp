1b9f1c2f06a325f0b9699e4faa3baf5545ea89de
Hamilton Little <hamilton.little@gmail.com>
2016-06-18 18:11:22 -0700

    reconfiguring sfml build deps.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,17 +1,20 @@
-#include "config.h"
+#include <config.h>
 #include <iostream>
-#include <SFML/Graphics.hp>
+#include <SFML/Graphics.hpp>
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Version " << shooty_face_VERSION_MAJOR << "." << shooty_face_VERSION_MINOR << std::endl;
+    std::cout << "Version " << shooty_face_VERSION_MAJOR
+              << "."        << shooty_face_VERSION_MINOR
+              << "."        << shooty_face_VERSION_REVIS
+              << std::endl;
 
     sf::Window app(sf::VideoMode(800, 600), "shooty_face");
 
     while(app.isOpen()) {
         sf::Event event;
-        while(App.pollEvent(event)) {
+        while(app.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 app.close();
             }
